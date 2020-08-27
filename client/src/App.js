8d87349c973768fa5c9ba8f7e28ba2db425c5e8d
Rachel Braband <rebraband@gmail.com>
2020-08-27 15:26:01 -0500

    commit on 'master'

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+// import '../styles/global.css';
+import { 
+  Switch,
+  BrowserRouter,
+  Route, } from 'react-router-dom';
 import './App.css';
 
+//componenet import
+import Courses from './Courses';
 
 
-
-import axios from 'axios';
-
-async function getUser() {
-  try {
-    const response = await axios.get('http://localhost:5000/api/courses');
-    console.log(response);
-  } catch (error) {
-    console.error(error);
-  } }
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  render () {
+    return (
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route path= '/Courses' render={ () => <Courses />} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
-getUser();
-export default App;
+
+export default (App);
---
+++ client/src/Courses.js
@@ -0,0 +1,89 @@
+//stateful
+/**This component provides the "Courses" screen by retrieving the list of 
+ * courses from the REST API's /api/courses route and rendering a list of courses.
+ *  Each course needs to link to its respective "Course Detail" screen.
+ *  This component also renders a link to the "Create Course" screen. */
+
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+class Courses extends Component {
+    constructor() {
+        super();
+        this.state = {
+            courses: []
+        };
+    }
+
+    //function to retrieve courses and store then in an array
+    async getCourses() {
+         axios.get('http://localhost:5000/api/courses')
+            .then(response => {this.setState({courses: response})})
+            .catch(error => console.log('Error fetching and parsing data', error));
+            console.log(this.state.courses);
+            }
+
+            
+    componentDidMount() {
+        this.getCourses();
+    }
+
+    render() {
+    let courses;
+    console.log(this.state.courses);
+    if(this.state.courses.length > 0) {
+        courses = this.state.courses.map((course) => 
+            <div className="grid-33" key={course.id}>
+                <a className="course--module course--link" href={'/courses/' + course.id}>
+                    <h4 className="course--label">Course</h4>
+                    <h3 className="course--title">{course.title}</h3>
+                </a>
+            </div>
+        );
+    }
+        
+
+
+    // render() {
+        return (
+            <div className="bounds">
+                {courses}
+                <div className="grid-33"><a className="course--module course--add--module" href="/courses/create">
+            <h3 className="course--add--title"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+                viewBox="0 0 13 13" className="add">
+                <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 "></polygon>
+              </svg>New Course</h3>
+          </a></div>
+      </div>
+        );
+    }
+}
+export default Courses;
+
+
+// import React, { useState, useEffect } from 'react';
+
+// function Courses() {
+//     const [data, setData] = useState ([]);
+
+//     useEffect(() => { 
+//         axios('http://localhost:5000/api/courses')
+//           .then(response => setData(response.data.data))
+//           .catch(error => console.log('Error fetching and parsing data', error))
+//       }, []);
+
+//       console.log(setData);
+//     return (
+//         <>
+//         <div className="bounds">
+//           <div className="grid-33">
+//             <h1 className="main-title">Courses</h1>
+//           </div>
+//         </div>
+//         <div className="main-content"></div>
+//       </>
+//     );
+// }
+
+// export default Courses;
