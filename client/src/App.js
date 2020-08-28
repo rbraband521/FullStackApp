38d098d08493fc953979fb25a483b5232155be3b
Rachel Braband <rebraband@gmail.com>
2020-08-28 13:03:06 -0500

    adding context signin

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,11 @@ import './App.css';
 //componenet import
 import Courses from './components/Courses';
 import CourseDetail from './components/CourseDetail';
+import UserSignIn from './components/UserSignIn';
+import withContext from './Context';
 
+
+const UserSignInWithContext = withContext(UserSignIn);
 
 
 class App extends Component {
@@ -20,8 +24,9 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <Switch>
-            <Route exact path= '/' render={ () => <Courses />} />
-            <Route path= '/courses/:id' render={ () => <CourseDetail />} />
+            <Route exact path= '/' component={Courses} />
+            <Route path= '/courses/:id' component={CourseDetail} />
+            <Route path="/signin" component={UserSignInWithContext} />
           </Switch>
         </div>
       </BrowserRouter>
---
+++ client/src/config.js
@@ -0,0 +1,4 @@
+
+export default {
+    apiBaseUrl: 'http://localhost:3000/api',
+  };
